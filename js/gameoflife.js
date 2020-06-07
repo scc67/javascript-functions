df852c74e0e06a3fc45c025fa93b4be06ee87bc5
Steve <anonymouse@bestaatniet.com>
2020-06-07 19:39:58 +0200

    getNeighborsOf af

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -47,7 +47,16 @@ const printCells = (state) => {
   return string;
 };
 
-const getNeighborsOf = ([x, y]) => {};
+const getNeighborsOf = ([x, y]) => [
+  [x - 1, y + 1],
+  [x, y + 1],
+  [x + 1, y + 1],
+  [x - 1, y],
+  [x + 1, y],
+  [x - 1, y - 1],
+  [x, y - 1],
+  [x + 1, y - 1]
+];
 
 const getLivingNeighbors = (cell, state) => {};
 
